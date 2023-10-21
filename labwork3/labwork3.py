47f9cd1b917ea939146c0076b9d3e292dcab34b2
ssjinkaido <tungxuannguyen16@gmail.com>
2023-10-21 15:21:31 +0700

    Renamed image and clean code

--- labwork3/labwork3.py
+++ labwork3/labwork3.py
@@ -34,7 +34,7 @@ def grayscale_gpu(src, dst):
 start = time.time()
 grayscale_image = grayscale_cpu(image)
 print(f"Time processed on CPU: {time.time() - start}s")
-
+cv2.imwrite(f"grayscale_image_cpu.png", grayscale_image)
 
 block_size = [64, 128, 256, 512, 1024]
 time_processed_per_block = []
@@ -52,9 +52,11 @@ for bs in block_size:
 
     output_host = output_device.copy_to_host()
     grayscale_image = np.reshape(output_host, (height, width))
-    cv2.imwrite(f"grayscale_image_cpu_{bs}.png", grayscale_image)
+    cv2.imwrite(f"grayscale_image_gpu_{bs}.png", grayscale_image)
 
-print(time_processed_per_block)
+for bs, t in zip(block_size, time_processed_per_block):
+    print(f"Block size: {bs}, time processed: {t}")
+
 plt.figure(figsize=(10, 5))
 plt.plot(block_size, time_processed_per_block, marker="o", linestyle="-", color="b")
 plt.title("Time Processed Per Block vs Block Size")
