dd02fc64aa71db6bb025d9a31c3508c2d0b07977
ssjinkaido <tungxuannguyen16@gmail.com>
2023-10-28 09:11:25 +0700

    Add argments parser to labwork6a

--- labwork6/labwork6a.py
+++ labwork6/labwork6a.py
@@ -3,15 +3,13 @@ import numpy as np
 import cv2
 import time
 import math
+import argparse
 
 image = cv2.imread("../images/image.jpeg")
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 height, width, _ = image.shape
 input_device = cuda.to_device(image)
-output_device = cuda.device_array(
-    (height, width, 3),
-    np.uint8,
-)
+output_device = cuda.device_array((height, width, 3), np.float32)
 
 
 @cuda.jit
@@ -27,20 +25,30 @@ def binarize(src, dst, threshold):
 
 block_size = [(8, 8), (16, 16), (24, 24), (32, 32)]
 time_processed_per_block = []
-threshold = 50
-for bs in block_size:
-    grid_size_x = math.ceil(width / bs[0])
-    grid_size_y = math.ceil(height / bs[1])
-    grid_size = (grid_size_x, grid_size_y)
-    start = time.time()
-    binarize[grid_size, bs](input_device, output_device, threshold)
 
-    time_processed = time.time() - start
-    time_processed_per_block.append(time_processed)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Labwork")
+    parser.add_argument(
+        "--threshold",
+        "-th",
+        type=int,
+        default=20,
+        help="Threshold value",
+    )
+    args = parser.parse_args()
 
-    output_host = output_device.copy_to_host()
-    # output_host = output_host * 255
-    cv2.imwrite(f"binarize_image.png", output_host)
+    for bs in block_size:
+        grid_size_x = math.ceil(width / bs[0])
+        grid_size_y = math.ceil(height / bs[1])
+        grid_size = (grid_size_x, grid_size_y)
+        start = time.time()
+        binarize[grid_size, bs](input_device, output_device, args.threshold)
 
-for bs, t in zip(block_size, time_processed_per_block):
-    print(f"Block size: {bs}, time processed: {t}")
+        time_processed = time.time() - start
+        time_processed_per_block.append(time_processed)
+
+        output_host = output_device.copy_to_host()
+        cv2.imwrite(f"binarize_image.png", output_host)
+
+    for bs, t in zip(block_size, time_processed_per_block):
+        print(f"Block size: {bs}, time processed: {t}")
